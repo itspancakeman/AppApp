68b50a7342c954d7da5b59d5283a15804d2cc73c
itspancakeman <collinjst.onge@gmail.com>
2024-08-08 00:54:05 -0400

    feat: fleshing out views for ingredients and recipes

--- appapp/urls.py
+++ appapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ingredient_list, name='ingredient_list'),
-    path('', views.recipe_list, name='recipe_list'),
+    path('ingredients/<int:pk>', views.ingredient_list, name='ingredient_list'),
+    path('recipes/<int:pk>', views.recipe_list, name='recipe_list'),
 ]
--- appapp/views.py
+++ appapp/views.py
@@ -10,3 +10,11 @@ def recipe_list(request):
     recipes = Recipe.objects.all()
     return render(request, 'appapp/recipe_list.html', {'recipes': recipes})
 
+def ingredient_detail(request, pk):
+    ingredient = Ingredient.objects.get(id=pk)
+    return render(request, 'appapp/ingredient_detail.html', {'ingredient': ingredient})
+
+def recipe_detail(request, pk):
+    recipe = Recipe.objects.get(id=pk)
+    return render(request, 'appapp/recipe_detail.html', {'recipe': recipe})
+
