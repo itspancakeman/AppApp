a85dba50221782316a7a8730e26de77dc12a67fb
itspancakeman <collinjst.onge@gmail.com>
2024-08-08 01:02:44 -0400

    feat: two functional views!

--- appapp/urls.py
+++ appapp/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('ingredients/<int:pk>', views.ingredient_list, name='ingredient_list'),
-    path('recipes/<int:pk>', views.recipe_list, name='recipe_list'),
+    path('ingredients', views.ingredient_list, name='ingredient_list'),
+    path('ingredients/<int:pk>', views.ingredient_detail, name='ingredient_detail'),
+    path('recipes', views.recipe_list, name='recipe_list'),
+    path('recipes/<int:pk>', views.recipe_detail, name='recipe_detail'),
 ]
