9dcca7f99702342ae5f94866515134f5b3045418
itspancakeman <collinjst.onge@gmail.com>
2024-08-07 22:44:38 -0400

    finishing up models

--- appapp/models.py
+++ appapp/models.py
@@ -2,26 +2,26 @@ from django.db import models
 
 class User(models.Model):
     email = models.CharField(max_length=50)
-    username = models.ChardField(max_length=25)
-    recpies = models.ForeignKey(Recipe, on_delete=models.CASCADE)
-    ratings = models.ForeignKey(Rating, on_delete=models.SET_NULL)
+    username = models.CharField(max_length=25)
+    recpies = models.ForeignKey("Recipe", on_delete=models.CASCADE)
+    ratings = models.ForeignKey("Rating", on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.username
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
-    created_by = models.ForeignKey(User, on_delete=models.SET_NULL)
-    ingredients = models.ManyToManyField(Ingredient)
-    ratings = models.ManyToManyField(Rating)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    ingredients = models.ManyToManyField("Ingredient")
+    rating = models.ManyToManyField("Rating", related_name="Rating")
 
     def __str__(self):
         return self.name
 
 class Rating(models.Model):
-    stars = models.IntegerField(max_length=5)
-    left_by = models.ForeignKey(User, on_delete=models.SET_NULL)
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    stars = models.IntegerField()
+    left_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="Recipe")
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=25)
