607ad7aaba0ba505d22b97c70f5696077325ffa4
itspancakeman <collinjst.onge@gmail.com>
2024-08-10 18:29:07 -0400

    commit push

--- appapp/settings.py
+++ appapp/settings.py
@@ -34,8 +34,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 
 ALLOWED_HOSTS = [
-    '',
-    '',
+    'appapp-l22v.onrender.com',
+    'https://appapp-l22v.onrender.com',
 ]
 
 
